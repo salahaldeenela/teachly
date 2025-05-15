5f7d8fa4caf127e547ae9e487af1ebb6d6a304e4
QasemAL <129743072+QasemAL@users.noreply.github.com>
2025-05-15 21:30:54 +0300

    SettingCalenderPages

---
+++ app/(app)/UsersPages/StudentCalenderPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const StudentCalender = () => {
+  return (
+    <View>
+      <Text>StudentCalender Component</Text>
+    </View>
+  );
+};
+
+export default StudentCalender;
---
+++ app/(app)/UsersPages/TutorCalenderPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const TutorCalender = () => {
+  return (
+    <View>
+      <Text>TutorCalender Component</Text>
+    </View>
+  );
+};
+
+export default TutorCalender;
